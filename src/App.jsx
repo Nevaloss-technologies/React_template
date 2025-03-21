cc817d623118b96c7bbbc4906c1298591ae15a16
Priyanka <priyanka.mparihar@gmail.com>
2025-03-21 16:45:18 +0530

    remove dist

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import { RouterProvider } from 'react-router-dom';
 
-// project imports
+// Project imports
 import router from 'routes';
 import ThemeCustomization from 'themes';
-
 import ScrollTop from 'components/ScrollTop';
 
 // ==============================|| APP - THEME, ROUTER, LOCAL ||============================== //
 
 export default function App() {
+  const apiUrl = import.meta.env.VITE_PUBLIC_URL;
+  const appVersion = import.meta.env.VITE_APP_VERSION;
+
+  console.log('API URL:', apiUrl);
+  console.log('App Version:', appVersion);
+
   return (
     <ThemeCustomization>
       <ScrollTop>
--- vite.config.mjs
+++ vite.config.mjs
@@ -3,26 +3,31 @@ import react from '@vitejs/plugin-react';
 import jsconfigPaths from 'vite-jsconfig-paths';
 
 export default defineConfig(({ mode }) => {
+  // Load environment variables based on the current mode (development, production, etc.)
   const env = loadEnv(mode, process.cwd(), '');
 
   return {
+    // Set the base path for the application
     base: env.VITE_APP_BASE_NAME || '/',
     server: {
-      open: true,
-      port: 3000,
-      host: true
+      open: true, // Automatically open the browser on server start
+      port: 3000, // Set the development server port
+      host: true  // Listen on all addresses, including LAN
     },
     preview: {
       open: true,
       host: true
     },
     define: {
-      global: 'window'
+      global: 'window' // Define global variables, if necessary
     },
-    plugins: [react(), jsconfigPaths()],
+    plugins: [
+      react(),          // Enable React support
+      jsconfigPaths()   // Resolve paths based on jsconfig.json
+    ],
     build: {
-      chunkSizeWarningLimit: 1000,
-      sourcemap: env.VITE_GENERATE_SOURCEMAP === 'true'
+      chunkSizeWarningLimit: 1000, // Set chunk size warning limit to 1000 kB
+      sourcemap: env.VITE_GENERATE_SOURCEMAP === 'true' // Enable or disable source maps
     }
   };
 });
